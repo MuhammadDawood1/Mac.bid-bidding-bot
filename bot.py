aa50dd113dad987287a7492cc0deaed4ae9b627c
= <=>
2023-03-10 04:38:25 +0500

    Basic bot is ready. The bot is placing bids without any errors

--- bot.py
+++ bot.py
@@ -1,1 +1,81 @@
+from extract import extractItems,driverFunc
+from selenium.webdriver.common.by import By
+from openpyxl import load_workbook
 import threading
+from time import sleep
+import datetime
+
+
+
+def getItemsFromExcel():
+    itemsWithPricesDict = {}
+    wb = load_workbook('itemsWithPrices.xlsx')
+    ws = wb['Sheet1']
+    itemsColumn = ws['A']
+    pricesColumn = ws['B']
+    for i in range(len(itemsColumn)):
+        itemsWithPricesDict[itemsColumn[i].value] = pricesColumn[i].value
+
+    #print(itemsWithPricesDict)    
+    return itemsWithPricesDict
+
+def bid(timeRemainingInSeconds,lotNumber):
+    
+    itemsWithPricesDict = getItemsFromExcel()
+    print(itemsWithPricesDict.get(lotNumber.strip()))
+    bidPrice = int(itemsWithPricesDict.get(lotNumber.strip()))
+    sleep(timeRemainingInSeconds -140)
+    driver = driverFunc()
+    driver.get(lotNumber.strip())
+    #listOfItems = extractItems(driver)
+    currentBid = None
+    bidButton = None
+    # for item in listOfItems:
+    #     if(item.get('lotNumber').strip() == lotNumber.strip()):
+    #         print(item.get('lotNumber').strip(),lotNumber.strip())
+    #         currentBid = item.get('currentBid')
+    #         bidButton = item.get('bidNowBtn')
+    #         break
+    bidButton = driver.find_element(by=By.XPATH, value='//*[@class="btn btn-primary btn-shadow btn-block"]')
+    currentBid = float(driver.find_element(by=By.XPATH, value='//*[@class="h1 font-weight-normal text-accent mb-0 mr-4"]/span').text.replace('$',''))
+    driver.execute_script("return arguments[0].scrollIntoView();", bidButton)
+    if(bidPrice >= currentBid):
+        bidButton.click()
+        try:
+            takeRiskBtn = driver.find_element(by=By.XPATH,value=('//*[@class="btn btn-danger mx-1 my-2 btn-block"]'))
+            takeRiskBtn.click()
+            
+        except:
+            try:
+                registerToBidBtn = driver.find_element(by=By.XPATH,value=('//*[@class="btn btn-block btn-primary"]'))
+                registerToBidBtn.click()
+                try:
+                    takeRiskBtn = driver.find_element(by=By.XPATH,value=('//*[@class="btn btn-danger mx-1 my-2 btn-block"]'))
+                    takeRiskBtn.click()
+                except Exception as e:
+                    print('take risk btn not found after register to bid btn')
+                        
+            except Exception as e:
+                print('regiter to bid btn not found')
+    sleep(7)
+    driver.close()
+
+def start():
+    threads = []
+    driver = driverFunc()
+    listOfItems = extractItems(driver)
+    driver.close()
+    seconds = 180
+    for item in listOfItems:
+        time = item.get('timeRemaining')
+        lotNumber = item.get('lotNumber').strip()
+        #print(lotNumber)
+        threads.append(threading.Thread(target=bid,args=(seconds,lotNumber)))
+        seconds = seconds + 60
+
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+start()
--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
+import datetime
+from time import sleep
 
 def driverFunc():
      #option = webdriver.ChromeOptions()
@@ -10,7 +12,7 @@ def driverFunc():
     #chromeOptions.add_argument('--headless')
     chromeOptions.add_argument('--disable-notifications')
 
-    chromeOptions.add_argument("window-size=1280,800")
+    #chromeOptions.add_argument("window-size=1280,800")
 
     chromeOptions.add_argument('--disable-extensions')
 
@@ -30,35 +32,46 @@ def driverFunc():
     driver.implicitly_wait(10)
     return driver
 
-driver = driverFunc()
-driver.get('https://www.mac.bid/account/watchlist')
-items = driver.find_elements(by=By.XPATH, value='//*[@class="card product-card d-block d-sm-flex flex-row"]')
 
-listOfItems = []
-for item in items:
-    itemDict = {}
-    itemDict['days'] = item.find_element(by=By.CLASS_NAME, value='cz-countdown-days').text
-    itemDict['hours'] = item.find_element(by=By.CLASS_NAME, value='cz-countdown-hours').text
-    itemDict['miniutes'] = item.find_element(by=By.CLASS_NAME, value='cz-countdown-minutes').text
-    #dropDown = item.find_element_by_tag_name('select')
-    #options = dropDown.find_elements_by_tag_name('option')
-    itemDict['lotNumber'] = item.find_element(by=By.TAG_NAME, value='a').get_attribute('href')
-    #itemDict['lotNumber'] = item.find_element(by=By.XPATH, value='//*[@class="product-meta d-block font-size-xs pb-1"]/a').get_attribute('href')
-    itemDict['currentBid'] = item.find_element(by=By.XPATH, value='//*[@class="h1 font-weight-normal text-accent mb-0 mr-4"]/span').text
-    #itemsDict['bidNowBtn'] = item.find_element(by=By.XPATH, value='//*[@class="btn btn-primary btn-shadow btn-block"]')
-    listOfItems.append(itemDict)
+def extractItems(driver):
+    
+    driver.get('https://www.mac.bid/account/watchlist')
+    sleep(5)
+    items = driver.find_elements(by=By.XPATH, value='//*[@class="card product-card d-block d-sm-flex flex-row"]')
+
+    listOfItems = []
+    for item in items:
+        itemDict = {}
+        days = int(list(item.find_element(by=By.CLASS_NAME, value='cz-countdown-days').text)[0])
+        hours = int(list(item.find_element(by=By.CLASS_NAME, value='cz-countdown-hours').text)[0])
+        miniutes = int(list(item.find_element(by=By.CLASS_NAME, value='cz-countdown-minutes').text)[0])
+        timeInSeconds = (days*24*60 + hours*60 + miniutes)*60 
+        itemDict['timeRmaining'] = timeInSeconds
+        #dropDown = item.find_element_by_tag_name('select')
+        #options = dropDown.find_elements_by_tag_name('option')
+        itemDict['lotNumber'] = item.find_element(by=By.TAG_NAME, value='a').get_attribute('href')
+        #itemDict['lotNumber'] = item.find_element(by=By.XPATH, value='//*[@class="product-meta d-block font-size-xs pb-1"]/a').get_attribute('href')
+        itemDict['currentBid'] = float(item.find_element(by=By.XPATH, value='//*[@class="h1 font-weight-normal text-accent mb-0 mr-4"]/span').text.replace('$',''))
+        itemDict['bidNowBtn'] = item.find_element(by=By.XPATH, value='//*[@class="btn btn-primary btn-shadow btn-block"]')
+        listOfItems.append(itemDict)
+
+    #print(listOfItems)
+    return(listOfItems)
 
 
-print(listOfItems)
-cardInWatchList = 'card product-card d-block d-sm-flex flex-row'
-addProtectionBtn = 'btn btn-success mx-1 my-2 btn-block' #btn btn-success mx-1 my-2 btn-block
-takeTheRiskBtn = 'btn btn-danger mx-1 my-2 btn-block' #btn btn-danger mx-1 my-2 btn-block
+
+#print(extractItems(driverFunc()))
+#print(listOfItems)
+# cardInWatchList = 'card product-card d-block d-sm-flex flex-row'
+# addProtectionBtn = 'btn btn-success mx-1 my-2 btn-block' #btn btn-success mx-1 my-2 btn-block
+# takeTheRiskBtn = 'btn btn-danger mx-1 my-2 btn-block' #btn btn-danger mx-1 my-2 btn-block
 
 
-outbidclass = 'alert alert-warning mb-2'
-winningClass = 'alert alert-dark font-size-sm mb-2 py-1'
-retailAndBidPrices = 'h1 font-weight-normal text-accent mb-0 mr-4'
-registertoBidBtnClass = 'btn btn-block btn-primary'
-registerToBidBtnText = 'Register to Bid'
+# outbidclass = 'alert alert-warning mb-2'
+# winningClass = 'alert alert-dark font-size-sm mb-2 py-1'
+# retailAndBidPrices = 'h1 font-weight-normal text-accent mb-0 mr-4'
+# registertoBidBtnClass = 'btn btn-block btn-primary'
+# registerToBidBtnText = 'Register to Bid'
 
-ModalClass = 'modal-content' #register to bid form class/see screen shot
+
+# ModalClass = 'modal-content' #register to bid form class/see screen shot
